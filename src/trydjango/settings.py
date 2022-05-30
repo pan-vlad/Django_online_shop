99c848f54c5c5cacb542e80e82119c4f05413d0a
Vladimir Panarin <panvlad2@mail.ru>
2022-05-30 22:58:10 +0400

    templates and templating engine basics

--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'trydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['C:/Users/panao/django_4_hours/src/templates/'],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
